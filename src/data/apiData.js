ab289708935af31eabfa71248c8ddb8a52d70f68
Cristian Soto <cristian.soto@vivint.com>
2026-04-20 01:31:06 -0400

    Move API update payload to shared test data

--- src/data/apiData.js
+++ src/data/apiData.js
@@ -4,6 +4,12 @@ const apiData = {
     body: 'This is a sample post created by an API test.',
     userId: 1,
   },
+  updatedPost: {
+    id: 1,
+    title: 'Updated Title',
+    body: 'Updated body content',
+    userId: 1,
+  },
 };
 
 module.exports = apiData;
--- tests/api/update-post.spec.js
+++ tests/api/update-post.spec.js
@@ -1,22 +1,16 @@
 const { test, expect } = require('@playwright/test');
 const { PostsApi } = require('../../src/api/postsApi');
 const { expectStatus } = require('../../src/api/apiAssertions');
+const apiData = require('../../src/data/apiData');
 
 test('@api update a post', async ({ request }) => {
   const postsApi = new PostsApi(request);
 
-  const updatedPayload = {
-    id: 1,
-    title: 'Updated Title',
-    body: 'Updated body content',
-    userId: 1,
-  };
-
-  const response = await postsApi.updatePost(1, updatedPayload);
+  const response = await postsApi.updatePost(1, apiData.updatedPost);
   expectStatus(response, 200);
 
   const body = await response.json();
 
-  expect(body.title).toBe(updatedPayload.title);
-  expect(body.body).toBe(updatedPayload.body);
+  expect(body.title).toBe(apiData.updatedPost.title);
+  expect(body.body).toBe(apiData.updatedPost.body);
 });
